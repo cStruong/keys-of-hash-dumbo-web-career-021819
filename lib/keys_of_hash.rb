473457f2ad8e2bb2aabd02de91f7062f878ed99a
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-02-16 18:44:06 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    finalArr = []
+    
+    animals.each{|element, value|
+      binding.pry
+    }
+    
+    return finalArr
   end
 end
